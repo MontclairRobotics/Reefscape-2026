eac822da9fd09c6be115c6489763614239098642
Bry <155580880+BCODERW@users.noreply.github.com>
2026-04-12 11:39:57 -0400

    fixed IDs

--- REEFSCAPE-2026/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ REEFSCAPE-2026/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -166,7 +166,7 @@ public class Drivetrain extends SubsystemBase {
                                                0.1); //Correct for skew that gets worse as angular velocity increases. Start with a coefficient of 0.1.
     swerveDrive.setModuleEncoderAutoSynchronize(false,
                                                 1); // Enable if you want to resynchronize your absolute encoders and motor encoders periodically when they are not moving.
-    // swerveDrive.pushOffsetsToEncoders(); // Set the absolute encoder to be used over the internal encoder and push the offsets onto it. Throws warning if not possible
+    swerveDrive.pushOffsetsToEncoders(); // Set the absolute encoder to be used over the internal encoder and push the offsets onto it. Throws warning if not possible
 
     boolean blueAlliance = DriverStation.getAlliance().isPresent() && DriverStation.getAlliance().get() == Alliance.Blue;
     Pose2d startingPose = blueAlliance ? new Pose2d(new Translation2d(Meter.of(1),
